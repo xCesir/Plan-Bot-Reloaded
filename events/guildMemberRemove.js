7996a94d71dab43bf65ff69820c7b3ead1e74499
xCesir <78450857+xCesir@users.noreply.github.com>
2025-03-23 19:00:55 +0100

    Kick message

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -5,7 +5,22 @@ module.exports = {
 	name: Events.GuildMemberRemove,
 	async execute(member) {
 		try {
-			goodbye(member);
+			console.log(`guildMemberRemove: ${member}`);
+			const fetchedLogs = await member.guild.fetchAuditLogs();
+			const kickLog = fetchedLogs.entries.first();
+			const { executor, target } = kickLog;
+			console.log(kickLog);
+
+			if (kickLog.createdAt < member.joinedAt) {
+				goodbye(member);
+				return console.log(`${member.user.tag} left the guild, most likely of their own will.`);
+			}
+			if (target.id === member.id) {
+				kickgoodbye(member, executor);
+				console.log(`${member.user.tag} left the guild; kicked by ${executor.tag}`);
+			} else {
+				console.log(`${member.user.tag} left the guild, audit log fetch was inconclusive.`);
+			}
 		}
 		catch (error) {
 			console.log(Date.now() + ': ' + 'An error occurred in module "guildMemberRemove":\n' + error);
@@ -19,6 +34,16 @@ function goodbye(member) {
 		.setTitle(goodbyeMessageTitle.replace('$(member)', member.user.username))
 		.setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })
 		.setDescription(goodbyeMessage.replace('$(member)', member.user.username));
+	member.guild.channels.cache.get(goodbyeChannelId).send({ embeds: [exampleEmbed] });
+
+}
+
+function kickgoodbye(member, executor) {
+	const exampleEmbed = new EmbedBuilder()
+		.setColor(0xe30825)
+		.setTitle(`${member.user.tag} has been kicked from the server.`)
+		.setAuthor({ name: member.user.username, iconURL: member.user.displayAvatarURL() })
+		.setDescription(`${member.user.tag} was kicked by ${executor.tag}.`);
 	member.guild.channels.cache.get(goodbyeChannelId).send({ embeds: [exampleEmbed] });
 
 }
